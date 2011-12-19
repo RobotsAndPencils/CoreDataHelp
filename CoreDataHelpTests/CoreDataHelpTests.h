8aca3f3543553c4fc80b5dea909c6b60cf03c5df
Drew Crawford <drew@sealedabstract.com>
2011-12-19 14:56:43 -0600

    Initial Commit

---
+++ CoreDataHelpTests/CoreDataHelpTests.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataHelpTests.h
+//  CoreDataHelpTests
+//
+//  Created by Drew Crawford on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CoreDataHelpTests : SenTestCase
+
+@end
---
+++ CoreDataHelpTests/CoreDataHelpTests.m
@@ -0,0 +1,32 @@
+//
+//  CoreDataHelpTests.m
+//  CoreDataHelpTests
+//
+//  Created by Drew Crawford on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreDataHelpTests.h"
+
+@implementation CoreDataHelpTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CoreDataHelpTests");
+}
+
+@end
