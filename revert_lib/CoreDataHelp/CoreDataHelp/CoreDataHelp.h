4cb18fb91d52a8c135ab44505423f63fbc62571a
Drew Crawford <drew@sealedabstract.com>
2012-02-03 17:41:20 -0600

    less magic framework

---
+++ revert_lib/CoreDataHelp/CoreDataHelp/CoreDataHelp.h
@@ -0,0 +1,13 @@
+//
+//  CoreDataHelp.h
+//  CoreDataHelp
+//
+//  Created by Drew Crawford on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoreDataHelp : NSObject
+
+@end
---
+++ revert_lib/CoreDataHelp/CoreDataHelp/CoreDataHelp.m
@@ -0,0 +1,23 @@
+//
+//  CoreDataHelp.m
+//  CoreDataHelp
+//
+//  Created by Drew Crawford on 2/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CoreDataHelp.h"
+
+@implementation CoreDataHelp
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
