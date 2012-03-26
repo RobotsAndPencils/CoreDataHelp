049dda21f771b2bf6c0e9f4f413e4d5b7bd6b24c
Drew Crawford <drew@sealedabstract.com>
2012-03-26 16:15:12 -0500

    working on install script

--- install.sh
+++ install.sh
@@ -6,18 +6,25 @@ TARGET_NAME=$PACKAGE_NAME
 STATIC_LIB=lib${PACKAGE_NAME}.a
 PATH_TO_PROJECT=ext/"$PACKAGE_NAME"/"$PROJECT_NAME"
 hash xsplice.rb 2>&- || { echo >&2 "I require xsplice.rb but it's not installed.  Grab it from https://github.com/drewcrawford/xsplice"; exit 1; }
-mkdir ext
-git submodule add "$GIT_URL" ext/"$PACKAGE_NAME"
-git submodule init
+
 echo "To which xcodeproject should I install this package?"
 read INSTALL_XCODEPROJ
-
+#INSTALL_XCODEPROJ="_iOS/GreenRoutine.xcodeproj"
+INSTALL_XCODEPROJ_DIR=`dirname "${INSTALL_XCODEPROJ}"`
 echo "To which target should I install this package?"
 read INSTALL_TARGET
+#INSTALL_TARGET="GreenRoutine"
+
+mkdir "$INSTALL_XCODEPROJ_DIR"/ext
+git submodule add "$GIT_URL" "$INSTALL_XCODEPROJ_DIR"/ext/"$PACKAGE_NAME"
+git submodule init
+
+
+
 xsplice.rb addproj --xcodeproj="$INSTALL_XCODEPROJ" --addproj=$PATH_TO_PROJECT
-xcodebuild clean
+pushd "$INSTALL_XCODEPROJ_DIR" && xcodebuild clean && popd
 xsplice.rb adddep --xcodeproj="$INSTALL_XCODEPROJ" --target="$INSTALL_TARGET" --foreignxcodeproj="$PROJECT_NAME" --foreigntarget="$TARGET_NAME"
-xcodebuild clean
+pushd "$INSTALL_XCODEPROJ_DIR" && xcodebuild clean && popd
 
 #setup fake framework build settings
 xsplice.rb setsettingarray --xcodeproj="$INSTALL_XCODEPROJ" --target="$INSTALL_TARGET" --setting_name="OTHER_LDFLAGS" --setting_value="-ObjC"
@@ -25,5 +32,11 @@ xsplice.rb setsettingarray --xcodeproj="$INSTALL_XCODEPROJ" --target="$INSTALL_T
 #autoconfig
 xsplice.rb autoconfig --xcodeproj="$INSTALL_XCODEPROJ"
 
+#force a reload
+killall 'Xcode'; open "$INSTALL_XCODEPROJ" && sleep 10 && killall 'Xcode'
+
+
 #link
-xsplice.rb linkstaticlib --xcodeproj="$PROJECT_NAME" --staticlib=${STATIC_LIB} --target="$TARGET_NAME"
+echo "linking to ${STATIC_LIB}"
+xsplice.rb linkstaticlib --xcodeproj="$INSTALL_XCODEPROJ" --staticlib=${STATIC_LIB} --target="$INSTALL_TARGET"
+killall 'Xcode'; open "$INSTALL_XCODEPROJ" && sleep 10 && killall 'Xcode'
